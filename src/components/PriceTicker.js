5c591aebdbfc3d1ea01ef7bc95a7eef4eee3e298
Ju99ernaut <brendonzee@gmail.com>
2021-03-29 14:52:35 +0200

    Format svgs

--- src/components/PriceTicker.js
+++ src/components/PriceTicker.js
@@ -9,15 +9,7 @@ const PriceTicker = ({ icon, coin, price, increase, change }) => {
                 <span className="coin-name">{coin}</span>
                 <div className="coin-price-container">
                     <p className="coin-price">{price}</p>
-                    <svg
-                        className={svgClass}
-                        fill="currentColor"
-                        width="20px"
-                        height="20px"
-                        focusable="false"
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
-                    >
+                    <svg className={svgClass} fill="currentColor" width="20px" height="20px" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
                         <path d="M7 10l5 5 5-5z"></path>
                     </svg>
                     <p className={percentageClass}>{change}</p>
