86d560ce70b4f61bf614495dd2cfbe38e1f5542e
Ju99ernaut <brendonzee@gmail.com>
2021-03-29 14:49:00 +0200

    Add user icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -65,6 +65,9 @@ class Navbar extends React.Component {
                     <div className="profile-container">
                         <button onClick={this.props.loginMd} className="btn btn-login">Login</button>
                         <button onClick={this.props.regMd} className="btn btn-login">Register</button>
+                        <svg viewBox="0 0 16 16" className="user-icon" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                        </svg>
                     </div>
                 </div>
             </div>
