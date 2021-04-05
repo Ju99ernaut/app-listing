8d6b0c27d49c3e8b4169a1640ba1f075a52ef968
Ju99ernaut <brendonzee@gmail.com>
2021-04-05 14:06:04 +0200

    Add fallback value

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,7 +2,7 @@ import ReactStarsRating from 'react-awesome-stars-rating';
 
 const Stars = ({ rating, edit, onChange }) => {
     return (
-        <ReactStarsRating isEdit={edit} onChange={onChange} value={rating} size={20} primaryColor="#ececec" />
+        <ReactStarsRating isEdit={edit} onChange={onChange} value={rating || 0} size={20} primaryColor="#ececec" />
     )
 };
 
