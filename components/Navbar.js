55c9ea468641e2a6f2d93ee4cdad2d40a1bab9ea
Ju99ernaut <brendonzee@gmail.com>
2021-05-19 13:39:44 +0200

    Fix links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PriceTicker from './PriceTicker';
 import Dropdown from './Dropdown';
+import Link from 'next/link';
 import fetch from '../utils/fetch';
 import querystring from 'querystring';
 import config from '../config';
@@ -73,7 +74,7 @@ class Navbar extends React.Component {
                 <div className="navbar">
                     <div className="logo-container">
                         <div className="logo">
-                            <a href="/"><img src="/rallyIcon.svg" alt="logo" /></a>
+                            <Link href="/"><a><img src="/rallyIcon.svg" alt="logo" /></a></Link>
                             <div>Marketplace</div>
                         </div>
                         <PriceTicker icon="/RLY.svg" coin="$RLY" price={this.state.price} increase={this.state.increase} change={this.state.change} />
