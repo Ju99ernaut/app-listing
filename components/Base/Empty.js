1ef0c9d01932a41bcc05e30ff0632c553957411e
Ju99ernaut <brendonzee@gmail.com>
2021-05-23 20:12:54 +0200

    Darken theme :gem:

--- components/Base/Empty.js
+++ components/Base/Empty.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Empty = () => {
     return (
         <div className="empty">
-            <Image width={150} height={150} src="/RLY.svg" alt="coin-icon" />
+            <Image width={80} height={80} src="/RLY.svg" alt="coin-icon" />
             <h2>Listings Empty</h2>
             <p>There are no listings under this category check again later</p>
         </div>
