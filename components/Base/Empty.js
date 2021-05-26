8097ad1be1489b3f311799d8160fc9250f9cd866
Ju99ernaut <brendonzee@gmail.com>
2021-05-27 00:45:32 +0200

    Fix typo

--- components/Base/Empty.js
+++ components/Base/Empty.js
@@ -1,7 +1,7 @@
 const Empty = () => {
     return (
         <div className="empty">
-            <Image width="80" height="80" src="/RLY.svg" alt="coin-icon" />
+            <img width="80" height="80" src="/RLY.svg" alt="coin-icon" />
             <h2>Listings Empty</h2>
             <p>There are no listings under this category check again later</p>
         </div>
